2e48b6596aa0e4e5152eb7823b7b20361767c386
parcidio <parcidioandre@gmail.com>
2024-05-14 03:03:19 +0100

    product detail page: button group

---
+++ lib/common/button_group/button_group.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:group_button/group_button.dart';
+
+import '../../utils/constants/colors.dart';
+import '../../utils/constants/sizes.dart';
+import '../../utils/helpers/helper_functions.dart';
+
+class AppButtonGroup extends StatelessWidget {
+  const AppButtonGroup({super.key, required this.itemList});
+
+final List itemList;
+  @override
+  Widget build(BuildContext context) {
+    final isDark = AppHelperFuncions.isDarkMode(context);
+    final controller = GroupButtonController();
+    return  Wrap(
+              spacing: 8,
+              children: [
+                GroupButton(
+                  isRadio: true,
+                  controller: controller,
+                  // onSelected: (index, isSelected) => print('$index button is selected'),
+                  buttons: itemList,
+                  options: GroupButtonOptions(
+  selectedShadow: const [],
+  selectedTextStyle: TextStyle(
+    fontSize: Theme.of(context).textTheme.titleMedium?.fontSize,
+    color: AppColors.white,
+  ),
+  selectedColor: AppColors.primary,
+  unselectedShadow: const [],
+  // unselectedColor: AppColors.tra,
+  unselectedTextStyle: TextStyle(
+    fontSize: Theme.of(context).textTheme.titleMedium?.fontSize,
+    color: isDark ? AppColors.light: AppColors.dark,
+  ),
+  selectedBorderColor: AppColors.secondary,
+  unselectedBorderColor: AppColors.darkGrey,
+  borderRadius: BorderRadius.circular(AppSizes.borderRadiusMd),
+  spacing: 10,
+  runSpacing: 10,
+  groupingType: GroupingType.wrap,
+  direction: Axis.horizontal,
+  buttonHeight: 30,
+  // buttonWidth: 30,
+  mainGroupAlignment: MainGroupAlignment.start,
+  crossGroupAlignment: CrossGroupAlignment.start,
+  groupRunAlignment: GroupRunAlignment.start,
+  textAlign: TextAlign.center,
+  textPadding: EdgeInsets.zero,
+  alignment: Alignment.center,
+  elevation: 0,
+),
+                  
+              )
+              ],
+            )
+          ;
+  }
+}
--- lib/presentation/shop/product_details/widgets/product_attributes.dart
+++ lib/presentation/shop/product_details/widgets/product_attributes.dart
@@ -1,3 +1,4 @@
+import 'package:dona/common/button_group/button_group.dart';
 import 'package:dona/common/widgets/chips/choice_chip.dart';
 import 'package:dona/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:dona/common/widgets/product/product_card/product_price_text.dart';
@@ -7,6 +8,7 @@ import 'package:dona/utils/constants/colors.dart';
 import 'package:dona/utils/constants/sizes.dart';
 import 'package:dona/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:group_button/group_button.dart';
 
 class AppProductAttribute extends StatelessWidget {
   const AppProductAttribute({super.key});
@@ -14,65 +16,24 @@ class AppProductAttribute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isDark = AppHelperFuncions.isDarkMode(context);
+    final controller = GroupButtonController();
     return Column(
       children: [
         // attributes
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const AppSectionHeading(title: 'Colors'),
-            const SizedBox(
-              height: AppSizes.spaceBetweenItems / 2,
-            ),
-            Wrap(
-              spacing: 8,
-              children: [
-                AppChoiceChip(
-                  text: 'Green',
-                  onSelected: (value) {},
-                  selected: true,
-                ),
-                AppChoiceChip(
-                  text: 'Red',
-                  onSelected: (value) {},
-                  selected: false,
-                ),
-                AppChoiceChip(
-                  text: 'Black',
-                  selected: false,
-                  onSelected: (value) {},
-                ),
-              ],
-            ),
-          ],
+            const AppSectionHeading(title: 'Tamanho'),           
+            AppButtonGroup(itemList: ["30", "32", "34", "40", "41", "42"])
+           ],
         ),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const AppSectionHeading(title: 'Sizes'),
-            const SizedBox(
-              height: AppSizes.spaceBetweenItems / 2,
-            ),
-            Wrap(spacing: 8, children: [
-              AppChoiceChip(
-                text: 'EU 30',
-                selected: true,
-                onSelected: (value) {},
-              ),
-              AppChoiceChip(
-                text: 'EU 32',
-                selected: false,
-                onSelected: (value) {},
-              ),
-              AppChoiceChip(
-                text: 'EU 34',
-                selected: false,
-                onSelected: (value) {},
-              ),
+             const AppSectionHeading(title: 'Cor'),           
+            AppButtonGroup(itemList: ["vermelho", "verde", "preto", "branco"])
             ]),
           ],
-        )
-      ],
     );
   }
 }
